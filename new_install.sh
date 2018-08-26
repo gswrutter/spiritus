f7b36e7b30f88d0463838e2a97f545f8c923076b
gswrutter <gswrutter@gmail.com>
2018-08-25 23:16:11 -0500

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -9,15 +9,21 @@
 
 ##### Set up commands to run in chroot environment #####
 # https://stackoverflow.com/questions/8157931/bash-executing-commands-from-within-a-chroot-and-switch-user
+# https://bbs.archlinux.org/viewtopic.php?id=154755
 cat << EOF | arch-chroot /mnt
 
 
-sudo pacman -Syu chromium
-
+sudo pacman -Syu chromium wget -y
 
 
 ##### Fix for Nemo's "Open Terminal Here" and Guake #####
 # https://medium.com/cognitio/open-guake-terminal-here-nemo-plugin-cd8e1af9ec0a
+# https://github.com/slgobinath/ubuntu-tweaks/blob/master/open-terminal-here/open-terminal-here.py
+# https://github.com/Guake/guake/issues/424
+# https://forums.linuxmint.com/viewtopic.php?t=240669
+# https://github.com/linuxmint/nemo-extensions/blob/master/nemo-python/README
+sudo mkdir /usr/share/nemo-python
+sudo mkdir /usr/share/nemo-python/extensions
 sudo wget https://raw.githubusercontent.com/slgobinath/ubuntu-tweaks/master/open-terminal-here/open-terminal-here.py --output-document /usr/share/nemo-python/extensions/open-terminal-here.py
 nemo -q
 
@@ -28,9 +34,18 @@ curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text
 
-
-
-
+##### Font - Iosevka #####
+# Will need to set this as system default font . . .  Somehow.
+# https://github.com/be5invis/Iosevka
+# https://aur.archlinux.org/packages/ttf-iosevka
+cd /home/$USERNAME/
+mkdir new_install
+cd new_install
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/ttf-iosevka.tar.gz
+untar
+unzip
+cd ttf-iosevka
+sudo makepkg -si ...
 
 
 EOF
